3c5600163497d9882518f87121b07c9d50f8976f
Jonathan Alexander Gibson <agibson@gibsonusa.net>
2019-07-11 13:37:30 -0500

    Added Username for user registration
    Database, migration table, and model changes for Username functionality

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,34 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
+    <div class="row">
+      <div class="col-3 p-5">
+        <img src="https://scontent-atl3-1.cdninstagram.com/vp/a67a299e0ec566398230a95489b39152/5DB5125D/t51.2885-19/s150x150/36967469_453622505113239_5983831497159737344_n.jpg?_nc_ht=scontent-atl3-1.cdninstagram.com" class="rounded-circle">
+      </div>
+      <div class="col-9 p-5">
+        <div class=""> <h1>Username</h1> </div>
+        <div class="d-flex py-2">
+          <div class="pr-5 "> <strong>123</strong> posts </div>
+          <div class="pr-5 "> <strong>456</strong> followers </div>
+          <div class="pr-5 "> <strong>789</strong> following </div>
         </div>
+        <div class=""> <strong>Bio Title</strong> </div>
+        <div class="">Bio</div>
+        <div class=""><strong><a href="#">website</a></strong></div>
+      </div>
+    </div>
+
+    <div class="row pt-5">
+      <div class="col-4">
+        <img src="https://scontent-atl3-1.cdninstagram.com/vp/de69cd8b34f866615bcb389217ed55de/5DB64462/t51.2885-15/e35/40893784_245257959382781_8110698046170226755_n.jpg?_nc_ht=scontent-atl3-1.cdninstagram.com" class="w-100">
+      </div>
+      <div class="col-4">
+        <img src="https://scontent-atl3-1.cdninstagram.com/vp/adef1aa6afefc09c500608f4f7b4bfac/5DA49070/t51.2885-15/sh0.08/e35/s750x750/37386354_216582085863661_4865903062503391232_n.jpg?_nc_ht=scontent-atl3-1.cdninstagram.com" class="w-100">
+      </div>
+      <div class="col-4">
+        <img src="https://scontent-atl3-1.cdninstagram.com/vp/fd08108bcfc543126e16570324b764ec/5DB4E833/t51.2885-15/e35/36161287_2015553048775015_1554685007606317056_n.jpg?_nc_ht=scontent-atl3-1.cdninstagram.com" class="w-100">
+      </div>
     </div>
 </div>
+
 @endsection
